4f583646b5736349409d2aa3acf0cf022275281a
Caio Zirretta <caiozirretta@hotmail.com>
2021-06-23 23:11:39 -0300

    Aula 04

--- main.js
+++ main.js
@@ -19,14 +19,61 @@ for(const link of links){
     })
 }
 
-/* Sombra no header */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function(){
+/* Muda a Header ao dar o Scroll */
+function changeHeaderWhenScroll(){
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
+    
     if(window.scrollY >= navHeight){
         header.classList.add('scroll')
     } else {
         header.classList.remove('scroll')
     }
+}
+
+/* Testimonials - Swiper/Carrousel */
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true
+})
+
+/* ScrollReveal */
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(`
+    #home .text, #home .image
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links,
+    footer .brand, footer .social
+    `
+, {interval:100})
+
+/* Botão back to top */
+
+function backToTop(){
+    const bttButton = document.querySelector('.back-to-top')
+    
+    if(window.scrollY >= 1100){
+        bttButton.classList.add('show')
+    } else {
+        bttButton.classList.remove('show')
+    }
+}
+
+/* No scroll */
+
+window.addEventListener('scroll', function(){
+    changeHeaderWhenScroll()
+    backToTop()
 })
